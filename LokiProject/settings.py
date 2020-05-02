33fa2aebd310a452dcb82084bbd57e9846fe333a
Your Name <you@example.com>
2020-05-02 16:21:08 +0530

    Heroku:shitty mistake in settings.py

--- LokiProject/settings.py
+++ LokiProject/settings.py
@@ -80,16 +80,18 @@ AUTHENTICATION_BACKENDS = (
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': 'postgres',
-# 		'USER': 'postgres',
-# 		'PASSWORD': 'jaxtek',
-# 		'HOST': 'localhost',
-# 		'PORT': '5432',
-# 	}
-# }
+
+# Do not comment this asshole
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'postgres',
+		'USER': 'postgres',
+		'PASSWORD': 'jaxtek',
+		'HOST': 'localhost',
+		'PORT': '5432',
+	}
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
